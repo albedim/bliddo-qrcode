dc36c497498baf728426c8d9edddfd7d2338455d
Alberto Di Maio <albertodimaio05@gmail.com>
2024-07-13 23:08:35 +0200

    first version

---
+++ src/components/qrcode/index.tsx
@@ -0,0 +1,8 @@
+import { QRCodeProps } from "../../types";
+import React from "react";
+
+export const QRCode: React.FC<QRCodeProps> = () => {
+    return (
+        <div>QRCode</div>
+    )
+}
---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export interface QRCodeProps {
+    value: string;
+    size?: number;
+    bgColor?: string;
+    fgColor?: string;
+    level?: 'L' | 'M' | 'Q' | 'H';
+    includeMargin?: boolean;
+    renderAs?: 'canvas' | 'svg';
+}
