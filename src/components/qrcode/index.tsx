5b1ec14c238d4c9d70d128fc91bf6c4e6c0f0e1c
Alberto Di Maio <albertodimaio05@gmail.com>
2024-07-13 23:29:37 +0200

    [build] - 1.0.1

--- src/components/qrcode/index.tsx
+++ src/components/qrcode/index.tsx
@@ -1,8 +1,39 @@
-import { QRCodeProps } from "../../types";
-import React from "react";
+import { QRCode } from "react-qrcode";
+import { BliddoQRCodeProps } from "../../types";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { BASE_URL } from "../../utils";
 
-export const QRCode: React.FC<QRCodeProps> = () => {
-    return (
-        <div>QRCode</div>
-    )
+export const BliddoQRCode: React.FC<BliddoQRCodeProps> = ( props ) => {
+
+  const [qrCodeFlowUrl, setQrCodeFlowUrl] = useState<string>("")
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+
+  const getCode = async () => {
+    await axios.get(BASE_URL + `/qrcodes/${props.qrCodeId}`)
+    .then(res => {
+      setQrCodeFlowUrl(res.data.flowUrl)
+      setIsLoading(false)
+    })
+    .catch(err => {
+      throw new Error(err.response.data.error.message)
+    })
+  }
+
+  useEffect(() => {
+    getCode()
+  }, [])
+
+  if (isLoading) {
+    return null
+  }
+  
+  return (
+    <QRCode
+      value={qrCodeFlowUrl}
+      size={props.size || 400}
+      bgColor={props.bgColor || "white"}
+      fgColor={props.fgColor || "black"}
+    />
+  )
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,11 @@
-export interface QRCodeProps {
-    value: string;
-    size?: number;
-    bgColor?: string;
-    fgColor?: string;
-    level?: 'L' | 'M' | 'Q' | 'H';
-    includeMargin?: boolean;
-    renderAs?: 'canvas' | 'svg';
+export interface BliddoQRCodeProps {
+    size?: number
+    bgColor?: string
+    fgColor?: string
+    /**
+     * The id of the qr code you want to track, 
+     * make sure you created it on bliddo.com and copied the id on our website 
+     * before using this component.
+     */
+    qrCodeId: string
 }
